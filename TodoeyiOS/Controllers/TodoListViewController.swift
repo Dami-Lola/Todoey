66dcf4680074489e78b7c1a894bef2c65856a4bc
Dami-lola <damiokafor@gmail.com>
2020-11-16 21:47:07 +0100

    User defaults restrictions

--- TodoeyiOS/Controllers/TodoListViewController.swift
+++ TodoeyiOS/Controllers/TodoListViewController.swift
@@ -9,55 +9,76 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    
     let defaults = UserDefaults.standard
-    var itemArray = ["Learn iOS Development","Build a good project","Apply for a job"]
+    var itemArray = [Item]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+        //        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+        //            itemArray = items
+        //        }
+        
+        
+        let newItem = Item()
+        newItem.title = "Miles Morals"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "TOnue eie"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "amel amamlee"
+        itemArray.append(newItem3)
+        
+        let newItem4 = Item()
+        newItem4.title = "Opleme Lmake"
+        itemArray.append(newItem4)
+        
+        if let items = defaults.array(forKey: "ToDoItemCell") as? [Item]{
             itemArray = items
         }
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
     //MARK:- UITableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let items = itemArray[indexPath.row]
-        
-
-        
+ 
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
+        let item = itemArray[indexPath.row]
+        
+        
+        let items = item.title
         cell.textLabel?.text = items
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         
         return cell
     }
     
-     //MARK:- UITableViewDelegate
+    //MARK:- UITableViewDelegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-   
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
         
     }
     
-     //MARK:- Add New Items
+    //MARK:- Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -68,7 +89,9 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
@@ -76,7 +99,7 @@ class TodoListViewController: UITableViewController {
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Creat new item"
             textField = alertTextField
-           
+            
         }
         
         alert.addAction(action)
---
+++ TodoeyiOS/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  TodoeyiOS
+//
+//  Created by Damilola Okafor on 11/16/20.
+//  Copyright © 2020 Damilola Okafor. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
